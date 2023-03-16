1aba7e5c6e81a5affe4a942d9eae0abfd9237584
phanilbb <phanilbb@gmail.com>
2023-03-16 22:45:52 +0530

    some fixes

--- SendCommunication/main.py
+++ SendCommunication/main.py
@@ -1,4 +1,4 @@
-from SendCommunication import send_communication
+import SendCommunication
 
 if __name__ == '__main__':
-    send_communication.SendCommunication(None, None)
+    SendCommunication.SendCommunication(None, None)
