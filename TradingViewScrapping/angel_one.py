bdea8ea9092613f6825a065c343774064c7f03fb
phanilbb <phanilbb@gmail.com>
2023-06-24 11:14:56 +0530

    ema stochastic stocks

---
+++ TradingViewScrapping/angel_one.py
@@ -0,0 +1,231 @@
+import json
+from boto3.dynamodb.conditions import Key, Attr
+import boto3
+from smartapi import SmartConnect
+import pyotp
+import time_helper
+import exception_handling
+
+trading_apis = {
+    'api_key': 'pSbQYzxm',
+    'secret_key': 'fda93c38-d97f-4078-96fb-271e2d52ea1f'
+}
+
+historical_apis = {
+    'api_key': 'dxZUmplW',
+    'secret_key': '678c3eef-6137-4206-a1f9-26ee5ad8f933'
+}
+
+EXCHANGE_TYPE = "NSE"
+INTERVAL = {
+    '3m': "THREE_MINUTE",
+    '30m': 'THIRTY_MINUTE',
+    '15m': 'FIFTEEN_MINUTE'
+}
+
+dynamodb = boto3.resource("dynamodb", region_name='ap-south-1')
+session_table = dynamodb.Table('Session')
+
+
+def get_session_data():
+    items = session_table.query(
+        KeyConditionExpression=Key("pk").eq(time_helper.get_current_date())
+    )
+    items_list = items.get("Items")
+    if not items_list:
+        return None
+
+    return items_list[0]
+
+
+def set_session_data(data):
+    data['pk'] = time_helper.get_current_date()
+    data['sk'] = time_helper.get_current_date()
+    session_table.put_item(Item=data)
+
+
+def create_session():
+    obj = SmartConnect(api_key=historical_apis['api_key'], access_token=historical_apis['secret_key'])
+    totp = pyotp.TOTP('M3SAGK57IJCJIB5GH5NFG7DITI')
+    data = obj.generateSession('L128041', '1703', str(totp.now()))
+    #set_session_data(data['data'])
+    print("new session generated : {}".format(json.dumps(data)))
+    return obj
+
+
+def get_session():
+    # get saved data in db
+    session_data = get_session_data()
+
+    if not session_data:
+        return create_session()
+
+    obj = SmartConnect(api_key=historical_apis['api_key'], access_token=historical_apis['secret_key'])
+    data = obj.generateSessionWithData(session_data)
+
+    if not data.get('status'):
+        return create_session()
+
+    print("session generated from DB : {}".format(json.dumps(data)))
+
+    return obj
+
+
+def historic_data(obj, token, fromdate, todate, interval):
+    historicParam = {
+        "exchange": EXCHANGE_TYPE,
+        "symboltoken": str(token),
+        "interval": INTERVAL[interval],
+        "fromdate": fromdate,
+        "todate": todate
+    }
+
+    try:
+        response = obj.getCandleData(historicParam)
+        if not response['status']:
+            print("Historic data api failed with status false")
+            return []
+
+        if not response['data']:
+            print("Historic data api failed with data null")
+            return []
+
+        print("Historic Response for token : {} is {}".format(str(token), json.dumps(response)))
+
+        return response['data']
+
+    except Exception as e:
+        print("Historic Api failed: {}".format(e))
+        exception_handling.send_message("Historic Api failed: {}".format(e))
+        return []
+
+
+def order_book(obj):
+    order_book = obj.orderBook()
+    order_book = [] if not order_book['data'] else order_book['data']
+    print("Order Book Response : {}".format(json.dumps(order_book)))
+    return order_book
+
+
+def place_market_order(obj, transaction_type, instrument_data):
+    orderparams = {
+        "variety": "NORMAL",
+        "tradingsymbol": instrument_data['symbol'],
+        "symboltoken": instrument_data['token'],
+        "transactiontype": transaction_type,
+        "exchange": EXCHANGE_TYPE,
+        "ordertype": "MARKET",
+        "producttype": "INTRADAY",
+        "duration": "DAY",
+        "quantity": instrument_data['quantity']
+    }
+    print(orderparams)
+    print("Market order params for symbol {} is {}".format(instrument_data['symbol'], json.dumps(orderparams)))
+    orderId = obj.placeOrder(orderparams)
+    print("The market order id for {} is: {}".format(instrument_data['symbol'], orderId))
+    return orderId
+
+
+def place_limit_order(obj, transaction_type, instrument_data, price):
+    orderparams = {
+        "variety": "NORMAL",
+        "tradingsymbol": instrument_data['symbol'],
+        "symboltoken": instrument_data['token'],
+        "transactiontype": transaction_type,
+        "exchange": EXCHANGE_TYPE,
+        "ordertype": "LIMIT",
+        "producttype": "INTRADAY",
+        "price": str(price),
+        "duration": "DAY",
+        "quantity": instrument_data['quantity']
+    }
+    print("Limit order params for symbol {} is {}".format(instrument_data['symbol'], json.dumps(orderparams)))
+    orderId = obj.placeOrder(orderparams)
+    print("The Limit order id for {} is: {}".format(instrument_data['symbol'], orderId))
+    return orderId
+
+
+def place_sl_order(obj, transaction_type, instrument_data, price):
+    orderparams = {
+        "variety": "STOPLOSS",
+        "tradingsymbol": instrument_data['symbol'],
+        "symboltoken": instrument_data['token'],
+        "transactiontype": transaction_type,
+        "exchange": EXCHANGE_TYPE,
+        "ordertype": "STOPLOSS_MARKET",
+        "producttype": "INTRADAY",
+        "triggerprice": str(price),
+        "duration": "DAY",
+        "quantity": instrument_data['quantity']
+    }
+    print("SL order params for symbol {} is {}".format(instrument_data['symbol'], json.dumps(orderparams)))
+    orderId = obj.placeOrder(orderparams)
+    print("The SL order id for {} is: {}".format(instrument_data['symbol'], orderId))
+    return orderId
+
+
+def place_sl_limit_order(obj, transaction_type, instrument_data, price):
+    triggerprice = float(price) + 2
+    if transaction_type == 'BUY':
+        triggerprice = float(price)
+        price = float(price) + 2
+
+    orderparams = {
+        "variety": "STOPLOSS",
+        "tradingsymbol": instrument_data['symbol'],
+        "symboltoken": instrument_data['token'],
+        "transactiontype": transaction_type,
+        "exchange": EXCHANGE_TYPE,
+        "ordertype": "STOPLOSS_LIMIT",
+        "producttype": "INTRADAY",
+        "triggerprice": str(triggerprice),
+        "price": str(price),
+        "duration": "DAY",
+        "quantity": instrument_data['quantity']
+    }
+    print("SL Limit order params for symbol {} is {}".format(instrument_data['symbol'], json.dumps(orderparams)))
+    orderId = obj.placeOrder(orderparams)
+    print("The SL Limit order id for {} is: {}".format(instrument_data['symbol'], orderId))
+    return orderId
+
+
+def cancel_order(obj, order_id, variety):
+    orderId = obj.cancelOrder(order_id, variety)
+    print("The Cancel order id is: {}".format(orderId))
+    return orderId
+
+
+def get_positions(obj):
+    r = obj.position()
+    print("The Positions response is: {}".format(json.dumps(r)))
+    p = [] if not r['data'] else r['data']
+    return p
+
+
+def modify_sl_limit_order_to_limit_order(obj, order, price):
+    order['ordertype'] = 'LIMIT'
+    order['variety'] = "NORMAL"
+    order['price'] = str(price)
+
+    response = obj.modifyOrder(order)
+
+    return response.get('status') == True
+
+
+def modify_limit_order_to_sl_limit_order(obj, order, price):
+    triggerprice = float(price) + 2
+    if order['transactiontype'] == 'BUY':
+        triggerprice = float(price)
+        price = float(price) + 2
+
+    order['ordertype'] = 'STOPLOSS_LIMIT'
+    order['variety'] = "STOPLOSS"
+    order['price'] = str(price)
+    order['triggerprice'] = str(triggerprice)
+
+    response = obj.modifyOrder(order)
+    return response.get('status') == True
+
+
+def get_ltp_data(obj, instrument_data):
+    return obj.ltpData(EXCHANGE_TYPE, instrument_data['sk'], instrument_data['token'])
---
+++ TradingViewScrapping/lambda_function.py
@@ -0,0 +1,148 @@
+import json
+import time
+
+import requests
+import time_helper
+import angel_one
+import numpy as np
+import pandas as pd
+
+
+def send_message(bot_message):
+    bot_token = '5945431317:AAFzROpE5IpiuiJyJJyXdCp7prE5-EH7mOg'
+    bot_chatID = '1170124746'
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+
+    response = requests.get(send_text)
+
+    return response.json()
+
+
+def lambda_handler(event, context):
+    obj = angel_one.create_session()
+    current_time = time_helper.get_current_time_with_delta(-5)
+    fromdate = time_helper.get_date_with_delta(-15) + " 09:15"
+    todate = time_helper.get_date_with_delta(0) + " " + current_time
+
+    f = open("trades.json")
+    data = json.load(f)
+
+    for each_data in data:
+
+        # if each_data['symbol'] != 'IRCTC-EQ':
+        #     continue
+
+        try:
+            print("check for trade : " + each_data['symbol'])
+
+            historic_data = angel_one.historic_data(obj, each_data['token'], fromdate, todate, "30m")
+            closes = [e[4] for e in historic_data]
+            ema_50 = calculate_ema(closes, 50)
+            ema_200 = calculate_ema(closes, 200)
+            rsi, k1, d1 = StochRSI(pd.Series(closes))
+            k_value = pd.Series.tolist(k1)[len(pd.Series.tolist(k1)) - 1] * 100
+            k_value_prev = pd.Series.tolist(k1)[len(pd.Series.tolist(k1)) - 2] * 100
+
+            ema_1 = ema_50[len(ema_50) - 1]
+            ema_2 = ema_200[len(ema_200) - 1]
+            print(
+                "EMA 50 for trade {trade} is {value}".format(trade=each_data['symbol'], value=ema_1))
+            print("EMA 200 for trade {trade} is {value}".format(trade=each_data['symbol'], value=ema_2))
+            print("Stochastic K value for trade {trade} is {value}".format(trade=each_data['symbol'], value=k_value))
+            print("Stochastic K prev value for trade {trade} is {value}".format(trade=each_data['symbol'],
+                                                                                value=k_value_prev))
+
+            if k_value_prev <= 20 and k_value > 20 and ema_1 < closes[len(closes) - 1] < ema_2:
+                print("Buy entry found for trade : {trade}".format(trade=each_data['symbol']))
+                send_message("Buy entry found for trade : {trade}".format(trade=each_data['symbol']))
+            elif k_value_prev >= 80 and k_value < 80 and ema_1 > closes[len(closes) - 1] > ema_2:
+                print("sell entry found for trade : {trade}".format(trade=each_data['symbol']))
+                send_message("sell entry found for trade : {trade}".format(trade=each_data['symbol']))
+            else:
+                print("NO entry found for trade : {trade}".format(trade=each_data['symbol']))
+        except Exception as e:
+            print("Exception for trade {trade} : {e}".format(trade=each_data['symbol'], e=e))
+
+        time.sleep(0.5)
+
+
+def calculate_ema(data, period):
+    # Calculate the smoothing factor
+    smoothing_factor = 2 / (period + 1)
+
+    # Calculate the initial SMA as the average of the first period values
+    sma = np.mean(data[:period])
+
+    ema_values = [sma]  # Store the EMA values
+
+    # Calculate EMA for the remaining data points
+    for i in range(period, len(data)):
+        ema = (data[i] - ema_values[-1]) * smoothing_factor + ema_values[-1]
+        ema_values.append(ema)
+
+    return ema_values
+
+
+def RSI(series, period=14):
+    delta = series.diff().dropna()
+    ups = delta * 0
+    downs = ups.copy()
+    ups[delta > 0] = delta[delta > 0]
+    downs[delta < 0] = -delta[delta < 0]
+    ups[ups.index[period - 1]] = np.mean(ups[:period])  # first value is sum of avg gains
+    ups = ups.drop(ups.index[:(period - 1)])
+    downs[downs.index[period - 1]] = np.mean(downs[:period])  # first value is sum of avg losses
+    downs = downs.drop(downs.index[:(period - 1)])
+    rs = ups.ewm(com=period - 1, min_periods=0, adjust=False, ignore_na=False).mean() / \
+         downs.ewm(com=period - 1, min_periods=0, adjust=False, ignore_na=False).mean()
+    return 100 - 100 / (1 + rs)
+
+
+# calculating Stoch RSI (gives the same values as TradingView)
+# https://www.tradingview.com/wiki/Stochastic_RSI_(STOCH_RSI)
+def StochRSI(series, period=14, smoothK=3, smoothD=3):
+    # Calculate RSI
+    delta = series.diff().dropna()
+    ups = delta * 0
+    downs = ups.copy()
+    ups[delta > 0] = delta[delta > 0]
+    downs[delta < 0] = -delta[delta < 0]
+    ups[ups.index[period - 1]] = np.mean(ups[:period])  # first value is sum of avg gains
+    ups = ups.drop(ups.index[:(period - 1)])
+    downs[downs.index[period - 1]] = np.mean(downs[:period])  # first value is sum of avg losses
+    downs = downs.drop(downs.index[:(period - 1)])
+    rs = ups.ewm(com=period - 1, min_periods=0, adjust=False, ignore_na=False).mean() / \
+         downs.ewm(com=period - 1, min_periods=0, adjust=False, ignore_na=False).mean()
+    rsi = 100 - 100 / (1 + rs)
+
+    # Calculate StochRSI
+    stochrsi = (rsi - rsi.rolling(period).min()) / (rsi.rolling(period).max() - rsi.rolling(period).min())
+    stochrsi_K = stochrsi.rolling(smoothK).mean()
+    stochrsi_D = stochrsi_K.rolling(smoothD).mean()
+
+    return stochrsi, stochrsi_K, stochrsi_D
+
+
+# calculating Stoch RSI
+#  -- Same as the above function but uses EMA, not SMA
+def StochRSI_EMA(series, period=14, smoothK=3, smoothD=3):
+    # Calculate RSI
+    delta = series.diff().dropna()
+    ups = delta * 0
+    downs = ups.copy()
+    ups[delta > 0] = delta[delta > 0]
+    downs[delta < 0] = -delta[delta < 0]
+    ups[ups.index[period - 1]] = np.mean(ups[:period])  # first value is sum of avg gains
+    ups = ups.drop(ups.index[:(period - 1)])
+    downs[downs.index[period - 1]] = np.mean(downs[:period])  # first value is sum of avg losses
+    downs = downs.drop(downs.index[:(period - 1)])
+    rs = ups.ewm(com=period - 1, min_periods=0, adjust=False, ignore_na=False).mean() / \
+         downs.ewm(com=period - 1, min_periods=0, adjust=False, ignore_na=False).mean()
+    rsi = 100 - 100 / (1 + rs)
+
+    # Calculate StochRSI
+    stochrsi = (rsi - rsi.rolling(period).min()) / (rsi.rolling(period).max() - rsi.rolling(period).min())
+    stochrsi_K = stochrsi.ewm(span=smoothK).mean()
+    stochrsi_D = stochrsi_K.ewm(span=smoothD).mean()
+
+    return stochrsi, stochrsi_K, stochrsi_D
