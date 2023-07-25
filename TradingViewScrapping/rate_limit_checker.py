4196acb18584d9f24688a58148e2119352e9ec7d
phanilbb <phanilbb@gmail.com>
2023-07-25 19:16:19 +0530

    rate limit fix

--- TradingViewScrapping/rate_limit_checker.py
+++ TradingViewScrapping/rate_limit_checker.py
@@ -1,26 +1,23 @@
 from datetime import datetime
 import time
-from collections import deque
 
 historic_api_rate_limit = 3
 historic_api_rate_limit_times = []
 
-request_times = deque(maxlen=3)
-
 
 def historic_rate_limit_checker():
-    current_time = time.time()
-    if len(request_times) == 3 and current_time - request_times[0] < 1:
-        # Calculate the time to sleep to meet the rate limit (3 requests per second)
-        time_to_sleep = 1 - (current_time - request_times[0])
-        time.sleep(time_to_sleep)
+    if len(historic_api_rate_limit_times) < historic_api_rate_limit:
+        historic_api_rate_limit_times.append(datetime.now())
+        return
 
-    request_times.append(current_time)
-
+    wait_time(max(historic_api_rate_limit_times), min(historic_api_rate_limit_times))
+    if historic_api_rate_limit_times:
+        historic_api_rate_limit_times.remove(min(historic_api_rate_limit_times))
+    historic_api_rate_limit_times.append(datetime.now())
 
 def wait_time(t2, t1):
     if (t2 - t1).microseconds * 0.001 * 0.001 < 1:
-        time.sleep(1 - (t2 - t1).microseconds * 0.001 * 0.001)
+        time.sleep(1 - (t2 - t1).microseconds * 0.001 * 0.001 + 0.1)
         global historic_api_rate_limit_times
         historic_api_rate_limit_times = []
 
