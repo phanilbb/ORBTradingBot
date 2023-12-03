6ec72e69423e4ffcdf619d3d4b3a542a435fba0f
phani173-acko <91243167+phani173-acko@users.noreply.github.com>
2023-12-03 17:11:18 +0530

    tags added

--- Instagram_1amStories/caption_generator.py
+++ Instagram_1amStories/caption_generator.py
@@ -11,7 +11,30 @@ CAPTION = """Like if you relate. Share if you love.
 {hashtags}
 .
 {author}
+—————————————————
+{tags}
 """
+
+TAGS = [
+    'healer1463',
+    'barlage6969',
+    'melissa.rose06',
+    'diordiorlitty',
+    'sarah.m_777',
+    'nadia_rose_19',
+    'st_acy2641',
+    'preciousphrincess',
+    'esraa.7537',
+    'az6rii',
+    'beautifulm101',
+    'abbydewitt12',
+    'sensualselfexplorer',
+    'vals_mundo.99',
+    'shalonda_knutson',
+    'bballkay28',
+    'game_over_rg',
+    'goldyglitter_collection'
+]
 
 
 def generate_caption(text, topic, author):
@@ -22,6 +45,8 @@ def generate_caption(text, topic, author):
 
     hashtags = hashtag_generator.generate_hashtags_from_text(text, topic)
 
-    caption = CAPTION.format(hashtags=hashtags, author=author)
+    tags = ['@' + each for each in TAGS]
+
+    caption = CAPTION.format(hashtags=hashtags, author=author, tags=' '.join(tags))
 
     return caption
