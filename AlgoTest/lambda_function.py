c5961afc5d227a371af1d06cd44f1369a25d7242
phani173-acko <91243167+phani173-acko@users.noreply.github.com>
2023-12-29 21:51:06 +0530

    input changes

--- AlgoTest/lambda_function.py
+++ AlgoTest/lambda_function.py
@@ -3,25 +3,36 @@ import angel_one_login
 import activate_strategy
 import communication
 import json
+import boto3
+
+ssm = boto3.client('ssm')
+
+
+def get_input_payload():
+    response = ssm.get_parameter(Name='algo_test_input', WithDecryption=False)
+    return json.loads(response['Parameter']['Value'])
 
 
 def lambda_handler(event, context):
-    with open('input.json', 'r') as file:
-        input_data = json.load(file)
-        accounts = input_data.get('accounts', [])
+    input_data = get_input_payload()
+    accounts = input_data.get('accounts', [])
 
-        for each_account in accounts:
-            result = {
-                'success': True,
-                'error': None,
-                'notify': True
-            }
-            for process in [login.run, angel_one_login.run, activate_strategy.run]:
-                if result['success']:
-                    process(each_account, result)
+    for each_account in accounts:
+        result = {
+            'success': True,
+            'error': None,
+            'notify': True
+        }
+        for process in [login.run, angel_one_login.run, activate_strategy.run]:
+            if result['success']:
+                process(each_account, result)
 
-            if result['success'] and result['notify']:
-                communication.telegram_bot_sendtext("{} - AlgoTest Successful".format(each_account['name']))
-            elif not result['success']:
-                communication.telegram_bot_sendtext(
-                    "{} - AlgoTest Failed : {}".format(each_account['name'], result['error']))
+        if result['success'] and result['notify']:
+            communication.telegram_bot_sendtext("{} - AlgoTest Successful".format(each_account['name']))
+        elif not result['success']:
+            communication.telegram_bot_sendtext(
+                "{} - AlgoTest Failed : {}".format(each_account['name'], result['error']))
+
+
+if __name__ == '__main__':
+    lambda_handler(None, None)
