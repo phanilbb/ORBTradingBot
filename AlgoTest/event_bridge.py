050f905f34a4a9795d02f1aa623ee609d1cb44f7
phani173-acko <91243167+phani173-acko@users.noreply.github.com>
2024-01-08 21:41:09 +0530

    rule enabling logs

--- AlgoTest/event_bridge.py
+++ AlgoTest/event_bridge.py
@@ -7,9 +7,11 @@ def start_second_eventbridge_rule():
     client = boto3.client('events')
     response = client.describe_rule(Name=SECOND_RULE)
     if response['State'] == 'DISABLED':
+        print("Enabling rule {}".format(SECOND_RULE))
         client.enable_rule(Name=SECOND_RULE)
 
 
 def stop_second_eventbridge_rule():
     client = boto3.client('events')
+    print("Disabling rule {}".format(SECOND_RULE))
     client.disable_rule(Name=SECOND_RULE)
