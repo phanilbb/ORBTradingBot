172a752b6e58c50efc22f1fc93e6b8cdac70f585
phani173-acko <91243167+phani173-acko@users.noreply.github.com>
2024-01-18 11:51:23 +0530

    plan subscription changes

--- AlgoTest/recharge.py
+++ AlgoTest/recharge.py
@@ -22,6 +22,29 @@ def get_plans(access_token_cookie, csrf_access_token):
     return r.json()
 
 
+def subscribe_plans(access_token_cookie, csrf_access_token):
+    payload = {
+        "plans": {
+            "external_connect": False,
+            "live_execution": {
+                "max_strategies": 2
+            }
+        }
+    }
+    headers = {
+        'Accept': 'application/json, text/plain, */*',
+        'Cookie': 'access_token_cookie=' + access_token_cookie + ';csrf_access_token=' + csrf_access_token,
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+        'X-CSRF-TOKEN-ACCESS': csrf_access_token
+    }
+
+    r = requests.post(url=URL, headers=headers, data=json.dumps(payload))
+    if r.status_code != 200:
+        raise errors.CustomError("Subscribe Plans failed {}".format(r.text))
+    print("Subscribe Plans data : " + json.dumps(r.json()))
+    return r.status_code == 200
+
+
 def run(account, result, combined_result):
     try:
         db_data = dynamo_db.get(account['name'])
@@ -33,16 +56,16 @@ def run(account, result, combined_result):
 
         plan_data = get_plans(login_data['access_token_cookie'], login_data['csrf_access_token'])
 
-        if not plan_data['expiration']:
-            print("Please recharge")
-        elif time_helper.is_before_current_time(plan_data['expiration']):
-            print("Plan expired on : {}".format(plan_data['expiration']))
+        if not plan_data['expiration'] or time_helper.is_before_current_time(plan_data['expiration']):
+            print("Plan Expired | Subscribing to plans")
+            if subscribe_plans(login_data['access_token_cookie'], login_data['csrf_access_token']):
+                db_data['plan_found'] = True
         else:
             print("Plan will expire on : {}".format(plan_data['expiration']))
             db_data['plan_found'] = True
-            dynamo_db.save_item(db_data)
+        dynamo_db.save_item(db_data)
 
     except Exception as e:
-        result['success'] = False
-        result['error'] = str(e)
-        return False
+        print("Recharge Exception : {}".format(str(e)))
+
+    return True
--- AlgoTest/time_helper.py
+++ AlgoTest/time_helper.py
@@ -8,4 +8,4 @@ def get_current_date():
 def is_before_current_time(target_date_str):
     current_date = datetime.now().date()
     target_date = datetime.strptime(target_date_str, '%Y-%m-%dT%H:%M:%S').date()
-    return current_date > target_date
+    return target_date < current_date
