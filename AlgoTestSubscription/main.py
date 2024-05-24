45d87271c1a8810fd261afd39fbaba14f4f77bdf
phani173-acko <91243167+phani173-acko@users.noreply.github.com>
2024-05-24 22:09:31 +0530

    renew algotest

--- AlgoTestSubscription/main.py
+++ AlgoTestSubscription/main.py
@@ -1,8 +1,4 @@
 from AlgoTestSubscription import lambda_function
 
 if __name__ == '__main__':
-    event = {
-        "phone_number": "+917013376537",
-        "password": "jVChDH5zMg2@h3"
-    }
-    lambda_function.lambda_handler(event, None)
+    lambda_function.lambda_handler(None, None)
